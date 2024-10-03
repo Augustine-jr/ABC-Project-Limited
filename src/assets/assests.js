cda952b66cce95051ca3f6c05b99153c1d4a6dfa
Ricch.dev <anekeaustin@gmail.com>
2024-10-03 14:06:43 +0100

    added new a picktrup_icon

--- src/assets/assests.js
+++ src/assets/assests.js
@@ -2,13 +2,17 @@ import ABC_Logo from './ABC.png';
 import ABC_light from './ABC_light.png';
 import search_icon from './search_icon.png';
 import profile_icon from './profile_icon.png';
+import pickuptruck_icon from './pickuptruck_icon.svg';
+import menu_icon from './menu_icon.svg';
 
 
 export const assets = {
     ABC_Logo,
     ABC_light,
     search_icon,
-    profile_icon
+    profile_icon,
+    pickuptruck_icon,
+    menu_icon
 }
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { assets } from '../assets/assests'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className={'flex justify-between w-full items-center py-5 px-6 font-bold bg-[#f3efef] text-gray-700'}>
+    <div className={'flex justify-between w-full items-center py-5 px-6 font-bold bg-[#ebe6d7] text-gray-700'}>
       
     <img src={assets.ABC_Logo } alt="ABC Logo" className='w-40' />
   
@@ -37,7 +37,19 @@ const Navbar = () => {
 
           <div className='group relative'>
             <img className='w-5 cursor-pointer'  src={assets.profile_icon} alt="Profile Icon"  />
+             <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+               <div className='flex flex-col gap-2 w-36 py-3 px-5 text-gray-500 rounded'>
+                 <p className='cursor-pointer hover:text-black'>My Profile</p>
+                  <p className='cursor-pointer hover:text-black'>Orders</p>
+                  <p className='cursor-pointer hover:text-black'>Logout</p>
+               </div>
+             </div>
           </div>
+           <Link to='/cart' className='relative'>
+            <img src={assets.pickuptruck_icon} alt="Picktruck Icon" className='w-8 min-w-5 cursor-pointer' />
+             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+           </Link>
+           <img src={assets.menu_icon} alt="Menu Icon" className='w-8 cursor-pointer' />
       </div>
     </div>
   )
