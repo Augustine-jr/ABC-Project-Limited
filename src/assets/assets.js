91da6980fa668bad25e6a7b0fd7863e34aec9b08
Ricch.dev <anekeaustin@gmail.com>
2024-10-06 06:56:31 +0100

    add a placeholder img for the right side of the hero section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,6 +6,7 @@ import pickuptruck_icon from './pickuptruck_icon.svg';
 import menu_icon from './menu_icon.svg';
 import backarrow_icon from './backarrow_icon.svg';
 import cancel_icon from './cancel_icon.svg';
+import placeholder_icon from './placeholder_icon.svg';
 
 
 export const assets = {
@@ -16,7 +17,10 @@ export const assets = {
     pickuptruck_icon,
     menu_icon,
     backarrow_icon,
-    cancel_icon
-}
+    cancel_icon,
+    placeholder_icon,
+ };
+
+
 
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
+    <div className='flex flex-col sm:flex-row border border-[#d1c7a3]'>
       {/* Hero left side */}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
         <div className='text-[#414141]'>
           <div className='flex items-center gap-2'>
             <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-            <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+            <p className='font-medium text-xs md:text-sm lg:text-base'>NAIL IT WITH OUR BESTSELLERS</p>
           </div>
-          <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+          <h1 className='text-xl sm:text-3xl lg:text-4xl leading-relaxed'>Timber For Every Project</h1>
           <div className='flex items-center gap-2'>
-            <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+            <p className='font-semibold text-xs md:text-sm lg:text-base'>SHOP THE TOOLS YOU NEED</p>
             <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
           </div>
         </div>
       </div>
+      {/* Hero right side */}
+       <img src={ assets.placeholder_icon } alt = "Placeholder Icon" className="w-full sm:w-1/2 object-cover" />
     </div>
   )
 }
