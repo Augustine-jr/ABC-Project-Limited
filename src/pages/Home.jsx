347b33dbec1fd53c34da12ddf6c0f235438fb9f9
Ricch.dev <anekeaustin@gmail.com>
2024-10-06 05:02:37 +0100

    create a Hero.jsx file under component,import it in the Home.jsx page,design the hero left side

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../components/Hero'// Imported this from Hero.jsx into the Home.jsx page so whenever we open HOme page it will display whatever is in Hero.jsx
 
 const Home = () => {
   return (
     <div>
-
+       <Hero />
     </div>
   )
 }
