c9ef419af50e393589cf41a55202d5d0bb0a3610
Augustine-jr <anekeaustin@gmail.com>
2024-11-29 23:52:41 +0100

    add TODO for backend

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [currentState, setCurrentState] = useState('Sign Up')
+
   return (
-    <div className="">Login</div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex itens-center gap-2 mb-2 mt-10'>
+          <p className='text-3xl'>{currentState}</p>
+      </div>
+      <input type='text' placeholder='Nmae' className='w-full px-3 py-2  border border-gray-800' required/>
+      <input type='email' placeholder='Email' className='w-full px-3 py-2  border border-gray-800' required/>
+      <input type='password' placeholder='Password' className='w-full px-3 py-2  border border-gray-800' required/>
+    </form>
   )
 }
 
