837eb6c413035ab38b97a71e3e52c074d8364b12
Augustine-jr <anekeaustin@gmail.com>
2024-12-05 01:37:53 +0100

    feat: add useState in login and signup page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const Login = () => {
 
-  const [currentState, setCurrentState] = useState('Sign Up')
+  const [currentState, setCurrentState] = useState('Login')
 
   return (
     <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex itens-center gap-2 mb-2 mt-10'>
           <p className='text-3xl'>{currentState}</p>
       </div>
-      <input type='text' placeholder='Nmae' className='w-full px-3 py-2  border border-gray-800' required/>
+      {currentState === 'Login' ? '' : <input type='text' placeholder='Name' className='w-full px-3 py-2  border border-gray-800' required/>}
       <input type='email' placeholder='Email' className='w-full px-3 py-2  border border-gray-800' required/>
       <input type='password' placeholder='Password' className='w-full px-3 py-2  border border-gray-800' required/>
     </form>
