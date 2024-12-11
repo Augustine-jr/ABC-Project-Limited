bb4159955916067276102906e8dc2aae9bf0da04
Augustine-jr <anekeaustin@gmail.com>
2024-12-11 12:51:26 +0100

    complete the Contact.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ import 'swiper/css/navigation'; // Optional: for navigation buttons/arrows
 import 'swiper/css/pagination'; // Optional: for pagination dots
 import { Navigation, Autoplay } from 'swiper/modules'; // Correctly import the modules
 import { assets } from '../assets/assets'
+import { ArrowRight, ArrowLeft } from 'lucide-react';
 
 
 const Hero = () => {
@@ -25,13 +26,13 @@ const Hero = () => {
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
         <div className='text-[#414141]'>
           <div className='flex items-center gap-2'>
-            <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+            <ArrowLeft className='text-gray-700 w-5 h-5' /> 
             <p className='font-medium text-xs md:text-sm lg:text-base'>NAIL IT WITH OUR BESTSELLERS.</p>
           </div>
           <h1 className='text-xl sm:text-3xl lg:text-4xl leading-relaxed font-semibold'>Timber For Every Project</h1>
           <div className='flex items-center gap-2'>
             <p className='font-semibold text-xs md:text-sm lg:text-base'>SHOP THE TOOLS YOU NEED</p>
-            <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+            <ArrowRight className='text-gray-700 w-5 h-5' /> 
           </div>
         </div>
       </div>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,21 +1,19 @@
-interface TitleProps {
-  text1: string;
-  text2: string;
-}
+import React from 'react';
+import { ArrowRight } from 'lucide-react';
 
-const Title: React.FC<TitleProps> = ({ text1, text2 }) => {
-  return (
-    <div className='inline-flex gap-2 items-center mb-3'>
+interface TitleProps { 
+  text1: string; 
+  text2: string; 
+} 
+
+const Title: React.FC<TitleProps> = ({ text1, text2 }) => { 
+  return ( 
+    <div className='inline-flex gap-2 items-center mb-3'> 
       <p className='font-semibold text-gray-700'>{text1}</p> 
       <p className='font-semibold text-gray-700'>{text2}</p>
-      <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-gray-700'></p>
-    </div>
-  );
-}
+      <ArrowRight className='text-gray-700 w-5 h-5' /> 
+    </div> 
+  ); 
+} 
 
 export default Title;
-
-
-
-
-
